e324f2b419c08dc40746005b873196cd0be629c8
Andres Alcocoer Urueta <a01567307@tec.mx>
2026-04-25 10:20:43 -0600

    ola

---
+++ Assets/Scripts/Drag_and_Drop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Drag_and_Drop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        transform.position = eventData.position;
+
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        transform.SetParent(parentAfterDrag);
+    }
+    
+}
